f652bc91ef119cf1c2927c260fb12d4a81e27dca
chriswa <chriswa@gmail.com>
2021-04-15 03:38:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# vsmod-PatchDebug
+
+Dumps JSON to logs before and after selected patches are made.
+
+Rename your patch file to include `.debug.` in its filename to trigger dumping. For example, `assets/ropeorganization/patches/ropetricks.debug.json`.
+
+Copious amounts of JSON will be dumped to your `server-debug.txt` and/or `client-debug.txt` log files.
+
+Please make sure to rename your files back when you're done!
+
+## Example
+
+`assets/ropeorganization/patches/ropetricks.debug.json`:
+
+```
+[
+  {
+    "file": "game:itemtypes/resource/rope.json",
+    "op": "add",
+    "path": "/creativeinventory",
+    "value": {
+      "made_of_plants": ["*"],
+      "long_and_thin": ["*"]
+    },
+  }
+]
+```
+
+Excerpt from `server-debug.txt`:
+
+```
+15.4.2021 03:29:36 [VerboseDebug] === PatchDebug === patches/ropetricks.debug.json => game:itemtypes/resource/rope.json #0 === BEFORE ===
+15.4.2021 03:29:36 [VerboseDebug] {
+	code: "rope",
+	texture: { base: "item/resource/rope" },
+	creativeinventory: { "general": ["*"], "items": ["*"] },
+	maxstacksize: 16,
+	materialDensity: 510
+}
+15.4.2021 03:29:36 [VerboseDebug] === PatchDebug === patches/ropetricks.debug.json => game:itemtypes/resource/rope.json #0 === /BEFORE ===
+15.4.2021 03:29:36 [VerboseDebug] === PatchDebug === patches/ropetricks.debug.json => game:itemtypes/resource/rope.json #0 === AFTER ===
+15.4.2021 03:29:36 [VerboseDebug] {
+  "code": "rope",
+  "texture": {
+    "base": "item/resource/rope"
+  },
+  "creativeinventory": {
+    "made_of_plants": [
+      "*"
+    ],
+    "long_and_thin": [
+      "*"
+    ]
+  },
+  "maxstacksize": 16,
+  "materialDensity": 510
+}
+15.4.2021 03:29:36 [VerboseDebug] === PatchDebug === patches/ropetricks.debug.json => game:itemtypes/resource/rope.json #0 === /AFTER ===
+```
+
+Oops! `/creativeinventory` got replaced, deleting `"general": ["*"], "items": ["*"]`.
+
+## Solution
+
+The purpose of this mod is to help identify problems, but for posterity, here is the correct way to solve the problem above of adding multiple keys:
+
+```
+[
+  {
+    "file": "game:itemtypes/resource/rope.json",
+    "op": "add",
+    "path": "/creativeinventory/made_of_plants",
+    "value": ["*"]
+  },
+  {
+    "file": "game:itemtypes/resource/rope.json",
+    "op": "add",
+    "path": "/creativeinventory/long_and_thin",
+    "value": ["*"]
+  }
+]
+```
---
+++ src/patch_ModJsonPatchLoader.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.ServerMods.NoObf;
+
+namespace PatchDebug {
+  [HarmonyPatch(typeof(ModJsonPatchLoader))]
+  [HarmonyPatch("ApplyPatch")]
+  public class Patch_ModJsonPatchLoader_ApplyPatch {
+    static IAsset GetPatchableAsset(AssetLocation patchSourcefile, JsonPatch jsonPatch, ICoreAPI api) {
+      // side skip logic from original function
+      if ((int)((!jsonPatch.Side.HasValue) ? jsonPatch.File.Category.SideType : jsonPatch.Side.Value) != 3 && jsonPatch.Side != (EnumAppSide?)api.Side) {
+        return null;
+      }
+
+      // skip wildcard patches (we'll get recursed anyways)
+      if (jsonPatch.File.Path.EndsWith("*")) { return null; }
+      // skip patch source files without ".debug." in the name
+      if (!patchSourcefile.Path.Contains(".debug.")) { return null; }
+
+      // load target asset
+      AssetLocation targetLocation = jsonPatch.File.Clone();
+      if (!targetLocation.Path.EndsWith(".json")) {
+        targetLocation.Path = targetLocation.Path + ".json";
+      }
+      IAsset targetAsset = api.Assets.TryGet(targetLocation, true);
+
+      return targetAsset;
+    }
+    static void Prefix(AssetLocation patchSourcefile, JsonPatch jsonPatch, int patchIndex, ICoreAPI ___api) {
+      var patchableAsset = GetPatchableAsset(patchSourcefile, jsonPatch, ___api);
+      if (patchableAsset != null) {
+        ___api.Logger.VerboseDebug("=== PatchDebug === {0} => {1} #{2} === BEFORE ===", patchSourcefile.Path, jsonPatch.File, patchIndex);
+        ___api.Logger.VerboseDebug("{0}", patchableAsset.ToText());
+        ___api.Logger.VerboseDebug("=== PatchDebug === {0} => {1} #{2} === /BEFORE ===", patchSourcefile.Path, jsonPatch.File, patchIndex);
+      }
+    }
+    static void Postfix(AssetLocation patchSourcefile, JsonPatch jsonPatch, int patchIndex, ICoreAPI ___api) {
+      var patchableAsset = GetPatchableAsset(patchSourcefile, jsonPatch, ___api);
+      if (patchableAsset != null) {
+        ___api.Logger.VerboseDebug("=== PatchDebug === {0} => {1} #{2} === AFTER ===", patchSourcefile.Path, jsonPatch.File, patchIndex);
+        ___api.Logger.VerboseDebug("{0}", patchableAsset.ToText());
+        ___api.Logger.VerboseDebug("=== PatchDebug === {0} => {1} #{2} === /AFTER ===", patchSourcefile.Path, jsonPatch.File, patchIndex);
+      }
+    }
+  }
+}
